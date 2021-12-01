626d55545bc70a028f9e366611869f5d1961b8fa
Waatze Goldewijk <github@goldewijk.net>
2021-12-01 23:05:02 +0100

    Solutions day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,30 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun getIncreaseCount(numbers: List<Int>): Int {
+        var previousValue: Int? = null
+        return numbers.count { currentValue ->
+            val valueIncreased = previousValue.let { it != null && it < currentValue }
+            previousValue = currentValue
+            valueIncreased
+        }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<Int>): Int {
+        return getIncreaseCount(input)
+    }
+
+    fun part2(input: List<Int>): Int {
+        val groups = input.mapIndexedNotNull() { index, _ ->
+            if (index + 2 > input.size - 1) {
+                null
+            } else {
+                input.slice(index..index + 2).sum()
+            }
+        }
+        return getIncreaseCount(groups)
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
+    val input = readInput("Day01").map(String::toInt)
     println(part1(input))
     println(part2(input))
 }
